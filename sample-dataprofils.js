844099473e3d03ecce61731b12f5794f1d6a1db9
Jérémy <legacjeremypro@gmail.com>
2026-06-05 13:03:23 +0200

    Add files via upload

---
+++ sample-dataprofils.js
@@ -0,0 +1,138 @@
+// Donnees fictives pour ProfileViewer.
+// Ces profils sont fournis uniquement pour tester l'application.
+
+window.Profils = [
+  {
+    reference: "DEMO-L40x40x2",
+    designation: "Corniere 40 x 40 - ep. 2",
+    largeur: 40,
+    hauteur: 40,
+    surface: 156,
+    poids: 0.421,
+    ixx: 20500,
+    iyy: 20500,
+    paths: "M0 0 H40 V2 H2 V40 H0 Z"
+  },
+  {
+    reference: "DEMO-L20x20x2",
+    designation: "Corniere 20 x 20 - ep. 2",
+    largeur: 20,
+    hauteur: 20,
+    surface: 76,
+    poids: 0.205,
+    ixx: 2600,
+    iyy: 2600,
+    paths: "M0 0 H20 V2 H2 V20 H0 Z"
+  },
+  {
+    reference: "DEMO-F40x2",
+    designation: "Meplat 40 x 2",
+    largeur: 40,
+    hauteur: 2,
+    surface: 80,
+    poids: 0.216,
+    ixx: 27,
+    iyy: 10667,
+    paths: "M0 0 H40 V2 H0 Z"
+  },
+  {
+    reference: "DEMO-F30x2",
+    designation: "Meplat 30 x 2",
+    largeur: 30,
+    hauteur: 2,
+    surface: 60,
+    poids: 0.162,
+    ixx: 20,
+    iyy: 4500,
+    paths: "M0 0 H30 V2 H0 Z"
+  },
+  {
+    reference: "DEMO-U30x20x2",
+    designation: "U 30 x 20 - ep. 2",
+    largeur: 20,
+    hauteur: 30,
+    surface: 136,
+    poids: 0.367,
+    ixx: 15600,
+    iyy: 5900,
+    paths: "M0 0 H20 V2 H2 V28 H20 V30 H0 Z"
+  },
+  {
+    reference: "DEMO-U20x20x2",
+    designation: "U 20 x 20 - ep. 2",
+    largeur: 20,
+    hauteur: 20,
+    surface: 96,
+    poids: 0.259,
+    ixx: 6200,
+    iyy: 4200,
+    paths: "M0 0 H20 V2 H2 V18 H20 V20 H0 Z"
+  },
+  {
+  reference: "DEMO-TC20x20x2",
+  designation: "Tube carre 20 x 20 - ep. 2",
+  largeur: 20,
+  hauteur: 20,
+  surface: 144,
+  poids: 0.389,
+  ixx: 7800,
+  iyy: 7800,
+  paths: "M0 0 H20 V20 H0 Z M2 2 V18 H18 V2 H2 Z"
+  },
+  {
+  reference: "DEMO-TR40x20x2",
+  designation: "Tube rectangulaire 40 x 20 - ep. 2",
+  largeur: 40,
+  hauteur: 20,
+  surface: 224,
+  poids: 0.605,
+  ixx: 11800,
+  iyy: 39500,
+  paths: "M0 0 H40 V20 H0 Z M2 2 V18 H38 V2 H2 Z"
+  },
+  {
+  reference: "DEMO-TC30x30x2",
+  designation: "Tube carre 30 x 30 - ep. 2",
+  largeur: 30,
+  hauteur: 30,
+  surface: 224,
+  poids: 0.605,
+  ixx: 29200,
+  iyy: 29200,
+  paths: "M0 0 H30 V30 H0 Z M2 2 V28 H28 V2 H2 Z"
+  },
+  {
+  reference: "DEMO-TC40x40x2",
+  designation: "Tube carre 40 x 40 - ep. 2",
+  largeur: 40,
+  hauteur: 40,
+  surface: 304,
+  poids: 0.821,
+  ixx: 73300,
+  iyy: 73300,
+  paths: "M0 0 H40 V40 H0 Z M2 2 V38 H38 V2 H2 Z"
+  },
+  {
+    reference: "DEMO-R40x2",
+    designation: "Tube rond diametre 40 - ep. 2",
+    largeur: 40,
+    hauteur: 40,
+    surface: 239,
+    poids: 0.645,
+    ixx: 43000,
+    iyy: 43000,
+    fillRule: "evenodd",
+    paths: "M40 20 A20 20 0 1 0 0 20 A20 20 0 1 0 40 20 Z M38 20 A18 18 0 1 1 2 20 A18 18 0 1 1 38 20 Z"
+  },
+  {
+    reference: "DEMO-T40x40x3",
+    designation: "Te 40 x 40 - ep. 3",
+    largeur: 40,
+    hauteur: 40,
+    surface: 231,
+    poids: 0.624,
+    ixx: 31500,
+    iyy: 41500,
+    paths: "M0 0 H40 V3 H21.5 V40 H18.5 V3 H0 Z"
+  }
+];
